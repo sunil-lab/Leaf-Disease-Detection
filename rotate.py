b11ae441a175880c0114fa14faeb94694007ca90
sunil <77413104+sunil-lab@users.noreply.github.com>
2021-08-20 13:21:16 +0530

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -9,7 +9,7 @@ cv2.imshow('Inverted', rotated_180_clockwise)
 cv2.imshow('270 deg', rotated_270_clockwise)
 
 print(
-" press O for to select Original \n press r for to select 90 deg \n press i for to select Inverted \n press u for to select 270 deg\n\n")
+" press O for to select Original \n press r for to select 90 deg \n press i for  to select Inverted \n press u for to select 270 deg\n\n")
 k = cv2.waitKey(0)
 if (k == 111):
     cv2.destroyAllWindows()
